889b7d491ff6c6df34943429d43382e71c43c9cd
daver <angelnmara@hotmail.com>
2018-03-23 00:59:37 -0600

    se agrega tabla horario semana

--- db/spGetHorariosXPeliculaXSucursal.sql
+++ db/spGetHorariosXPeliculaXSucursal.sql
@@ -1,3 +1,5 @@
+SET GLOBAL group_concat_max_len=65000;
+
 use dbmadeinchiconcuac;
 
 drop procedure if exists spGetHorariosXPeliculaXSucursal;
@@ -6,12 +8,15 @@ DELIMITER ;;
 CREATE DEFINER=CURRENT_USER PROCEDURE spGetHorariosXPeliculaXSucursal(pelicula int, sucursal int)
 BEGIN
         
-    select distinct CONCAT(
+    select 
+    CONCAT(
     '{"spGetHorariosXPeliculaXSucursal": [', 
-    GROUP_CONCAT(JSON_OBJECT('fiIdSucursal', fiIdSucursal, 
+    GROUP_CONCAT(
+    JSON_OBJECT('fiIdSucursal', fiIdSucursal, 
 						'fiIdPelicula', fiIdPelicula, 
+                        'fcSalaNom', fcSalaNom,
                         'fiIdHora', c.fiIdHora, 
-                        'fdHora', fdHora)),
+                        'fdHora', date_format(fdHora, '%H:%i'))),
     ']}'
 	) as salida
 	from tbSucursalSalaPelicula a
@@ -19,11 +24,14 @@ BEGIN
     on a.fiIdSucursalSalaPelicula = b.fiIdSucursalSalaPelicula
     inner join tbhoras c
     on b.fiIdHora = c.fiIdHora
+    inner join tbSalas d
+    on a.fiIdSala = d.fiIdSala
     where fiIdSucursal = sucursal
-    and fiIdPelicula = pelicula;
+    and fiIdPelicula = pelicula
+    order by fdHora;
     
 END;;
 DELIMITER ;
 
 
-call spGetHorariosXPeliculaXSucursal(2, 3);
+call spGetHorariosXPeliculaXSucursal(3,3);
